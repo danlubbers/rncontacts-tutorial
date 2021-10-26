c49eb6fbc6aa6bf416bd30cf2f7a113b62bb0a6f
Dan Lubbers <lubbers.dan@gmail.com>
2021-10-26 16:44:57 -0400

    Finished the design of Contact Detail Screen

--- src/components/ContactDetails/ContactDetails.js
+++ src/components/ContactDetails/ContactDetails.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  Image,
-  FlatList,
-  ScrollView,
-  TouchableOpacity,
-  ActivityIndicator,
-} from 'react-native';
+import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
 import styles from './ContactDetailsStyles';
 import colors from '../../assets/theme/colors';
-
 import ImageComponent from '../Image/Image';
+import CustomButton from '../CustomButton/CustomButton';
 import Icon from '../Icon/Icon';
 import {useNavigation} from '@react-navigation/native';
+import {CREATE_CONTACT} from '../../constants/routeNames';
 
 const ContactDetails = ({contactDetails}) => {
   const {navigate} = useNavigation();
@@ -27,6 +20,79 @@ const ContactDetails = ({contactDetails}) => {
           <Text style={styles.fullName}>
             {first_name} {last_name}
           </Text>
+          <View style={styles.hrLine} />
+
+          <View style={styles.topCallOptions}>
+            <TouchableOpacity style={styles.topCallOption}>
+              <Icon
+                type="ionicon"
+                name="call-outline"
+                color={colors.primary}
+                size={27}
+              />
+              <Text style={styles.middleText}>Call</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.topCallOption}>
+              <Icon
+                type="materialCommunity"
+                name="message-text"
+                color={colors.primary}
+                size={27}
+              />
+              <Text style={styles.middleText}>Text</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.topCallOption}>
+              <Icon
+                type="materialCommunity"
+                name="video"
+                color={colors.primary}
+                size={27}
+              />
+              <Text style={styles.middleText}>Video</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.middleCallOptions}>
+            <Icon
+              type="ionicon"
+              name="call-outline"
+              color={colors.grey}
+              size={27}
+            />
+            <View style={styles.phoneMobile}>
+              <Text>{phone_number}</Text>
+              <Text>Mobile</Text>
+            </View>
+
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Icon
+                type="materialCommunity"
+                name="video"
+                color={colors.primary}
+                size={27}
+              />
+              <Icon
+                type="materialCommunity"
+                name="message-text"
+                color={colors.primary}
+                size={27}
+                style={[styles.msgIcon]}
+              />
+            </View>
+          </View>
+          <CustomButton
+            style={{alignSelf: 'flex-end', marginRight: 20, width: 200}}
+            primary
+            title="Edit Contact"
+            onPress={() => {
+              navigate(CREATE_CONTACT, {contactDetails, editing: true});
+            }}
+          />
         </View>
       </View>
     </ScrollView>
--- src/components/ContactDetails/ContactDetailsStyles.js
+++ src/components/ContactDetails/ContactDetailsStyles.js
@@ -15,4 +15,45 @@ export default StyleSheet.create({
     fontSize: 27,
     fontWeight: 'bold',
   },
+  hrLine: {
+    height: 10,
+    borderColor: colors.grey,
+    borderBottomWidth: 0.4,
+  },
+
+  topCallOptions: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+
+  topCallOption: {
+    alignItems: 'center',
+  },
+
+  middleText: {
+    fontSize: 14,
+    color: colors.primary,
+    paddingVertical: 5,
+  },
+
+  middleCallOptions: {
+    flexDirection: 'row',
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+  },
+
+  phoneMobile: {
+    flexGrow: 1,
+    paddingHorizontal: 20,
+  },
+
+  imageView: {
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+    alignSelf: 'center',
+  },
 });
