26335837d12eb0f9da96fe71ce136a701a7cc0a3
Dan Lubbers <lubbers.dan@gmail.com>
2021-10-27 18:40:39 -0400

    Added header options to contact detail page: favorite icon and delete icon

--- src/screens/contactDetails.js
+++ src/screens/contactDetails.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import {useRoute} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {View, TouchableOpacity} from 'react-native';
+import Icon from '../components/Icon/Icon';
+import {useRoute, useNavigation} from '@react-navigation/native';
 import ContactDetailsComponent from '../components/ContactDetails/ContactDetails';
+import colors from '../assets/theme/colors';
 
 const ContactDetails = () => {
   const {params} = useRoute();
+  const {setOptions} = useNavigation();
+
+  useEffect(() => {
+    if (params.item) {
+      setOptions({
+        title: `${params.item.first_name} ${params.item.last_name}`,
+        headerRight: () => {
+          return (
+            <View style={{flexDirection: 'row', paddingRight: 10}}>
+              <TouchableOpacity>
+                <Icon
+                  name={params.item.is_favorite ? 'star' : 'star-border'}
+                  type="material"
+                  size={21}
+                  color={colors.grey}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity style={{paddingLeft: 10}}>
+                <Icon
+                  name="delete"
+                  type="material"
+                  size={21}
+                  color={colors.grey}
+                />
+              </TouchableOpacity>
+            </View>
+          );
+        },
+      });
+    }
+  }, [params.item]);
 
   return <ContactDetailsComponent contactDetails={params.item} />;
 };
